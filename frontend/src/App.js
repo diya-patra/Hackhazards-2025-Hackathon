bd5cb2975e38913cba56e4148ce98c8079591457
Diya Patra <diyapatra2nd@gmail.com>
2025-04-14 21:43:11 +0530

    Created a basic app using React

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={styles.container}>
+      <h1>Welcome to Hackhazards 2025!</h1>
+      <p>This is your starting point for building the best web app. 🚀</p>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "50px",
+    fontFamily: "Arial, sans-serif"
+  },
+};
+
+export default App;
